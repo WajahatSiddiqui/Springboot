320e96d76d230da1ebcf210ba5abd93bb0b568f3
Wajahat <wajahat.siddiqui22@gmail.com>
2017-07-14 00:18:27 +0530

    update customer controller
    Signed-off-by: Wajahat <wajahat.siddiqui22@gmail.com>

--- springboot-lombok/src/main/java/com/wajahat/learn/springboot/lombok/springbootlombok/CustomerController.java
+++ springboot-lombok/src/main/java/com/wajahat/learn/springboot/lombok/springbootlombok/CustomerController.java
@@ -2,13 +2,14 @@ package com.wajahat.learn.springboot.lombok.springbootlombok;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
